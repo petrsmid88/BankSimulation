72d3e4a669cbac191cfb965771dedbcde81b3c29
petrsmid88 <petrsmid@post.cz>
2020-02-04 08:55:07 +0100

    Create Project
    1st commit

---
+++ Bank/Bank/Objects/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bank.Types;
+
+namespace Bank.Objects
+{
+    class Customer : User
+    {
+        private String SSN { get; set; }
+
+        private String Password { get; set; }
+
+        private Boolean Valid { get; set; }
+
+        private CustomerType CustomerType { get; set; }
+
+    }
+}
---
+++ Bank/Bank/Objects/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Objects
+{
+    class User
+    {
+        protected String Name { get; set; }
+        protected String Surname { get; set; }
+        protected Address Address { get; set; }
+        protected String Mail { get; set; }
+        protected String Phone { get; set; }
+
+
+
+    }
+}
